3ee607c47583a74e6c09c05e387db9b4f47ac924
Pedro <peredaarriolapedroandres@gmail.com>
2026-04-16 15:22:05 -0600

    Agregando frontend completo como archivos normales

---
+++ frontend/biblioapp-frontend/login/logincomponent.css/Untitled-2.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      // Fake authentication
+      this.router.navigate(['/catalogo']);
+    }
+  }
+}
---
+++ frontend/biblioapp-frontend/login/logincomponent.html/Untitled-2.html
@@ -0,0 +1,24 @@
+<h2>Login</h2>
+
+<form [formGroup]="loginForm" (ngSubmit)="login()">
+
+  <mat-form-field appearance="fill">
+    <mat-label>Email</mat-label>
+    <input matInput formControlName="email">
+  </mat-form-field>
+
+  <br />
+
+  <mat-form-field appearance="fill">
+    <mat-label>Contraseña</mat-label>
+    <input matInput type="password" formControlName="password">
+  </mat-form-field>
+
+  <br />
+
+  <button mat-raised-button color="primary"
+          [disabled]="loginForm.invalid">
+    Ingresar
+  </button>
+
+</form>
